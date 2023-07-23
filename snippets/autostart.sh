844171b9ffef8f271f441523a8cc38961e268cfa
jb <johannes.bouche@fb2.fra-uas.de>
2023-07-23 20:26:25 +0200

    Add volumebuttons

--- snippets/autostart.sh
+++ snippets/autostart.sh
@@ -7,7 +7,7 @@ $SNIPPETS/autostart_clean.sh
 # Force always
 #xrandr -s 1680x1050 &
 #xrandr -s 1920x1080 &
-xrandr --output DP-1 --primary --auto --mode 1920x1080 --pos 0x0 --rotate normal \
+xrandr --output DP-2 --primary --auto --mode 1920x1080 --pos 0x0 --rotate normal \
     --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal
 
 xset s off &

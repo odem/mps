b1353186adb52ee2dbefcf76adb6df6c91a16005
jb <johannes.bouche@fb2.fra-uas.de>
2024-01-09 20:44:23 +0100

    Migrate some nuro aliases

--- dotfiles/.custom_alias.sh
+++ dotfiles/.custom_alias.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 # -- NURODEV -----------------------------------------------------------------
-WEBDIR=/home/design-daas/backend/web
+# Alias variables
+# NOTE: Expand when defined, not when used!
+# NOTE: Consider escaping
+REPODIR=/home/jb/mps/repo/github/nuromedia/design-daas
+BACKENDDIR=$REPODIR/backend
+WEBDIR=$REPODIR/web
+NOWEBDIR=$BACKENDDIR/noweb
+ISODIR=$BACKENDDIR/installer/iso
 # Directories
+alias dir-nuro="z $REPODIR"
+alias dir-backend="z $BACKENDDIR"
 alias dir-web="z $WEBDIR"
+alias dir-noweb="z $NOWEBDIR"
+alias dir-iso="z $ISODIR"
 # Service control
 alias srv-demo="z $WEBDIR ; make demo"
 alias srv-serve="z $WEBDIR ; make serve"
 # Other
 alias vq="z $WEBDIR ; nvim daas_web/quart_app.py"
-alias backend="z ~/mps/repo/github/nuromedia/design-daas/backend"
-alias nuro=" z ~/mps/repo/github/nuromedia"
 alias nuromux="tmuxinator stop nuro ; tmuxinator start nuro"
 
